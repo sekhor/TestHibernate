be95bc8581e7640a191a98b5f9c45baeacd1e6bb
KHOR SWEE ENG <sekhor@gmail.com>
2013-05-30 09:00:19 +0800

    chk in

---
+++ Helloworld2/src/mypage.java
@@ -0,0 +1,52 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class mypage
+ */
+@WebServlet(description = "my page", urlPatterns = { "/mypage" })
+public class mypage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public mypage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+	    PrintWriter out = response.getWriter();
+	    out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " +
+	                                        "Transitional//EN\">\n" +
+	                "<HTML>\n" +
+	                "<HEAD><TITLE>Hello WWW</TITLE></HEAD>\n" +
+	                "<BODY>\n" +
+	                "<H1>Hello WWW</H1>\n" +
+	                "</BODY></HTML>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
